be5c39021f25fdcf2c28b4cee64ae8cb6da1a421
VadimTalimonchik <tvadmins@gmail.com>
2022-07-07 15:06:56 +0300

    seminar 1 homework task 2solution

--- sem1_hw_task2/Program.cs
+++ sem1_hw_task2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 2
+
+Напишите программу, которая на вход
+принимает два числа и выдаёт, какое
+число большее, а какое меньшее.
+*/
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine("Число А больше чила В");
+}
+else
+{
+    Console.WriteLine("Число В больше чила А");
+}
+
