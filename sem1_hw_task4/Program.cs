6fc6f553c9f055b55d604db10aa7ce856c6325a2
VadimTalimonchik <tvadmins@gmail.com>
2022-07-07 15:38:41 +0300

    seminar 1 homework task 4solution

--- sem1_hw_task4/Program.cs
+++ sem1_hw_task4/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 4
+
+Напишите программу, которая принимает
+на вход три числа и выдаёт максимальное
+из этих чисел.
+*/
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число С: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (max < numberB)
+{
+    max = numberB;
+}
+if (max < numberC)
+{
+    max = numberC;
+}
+Console.WriteLine("Максимальное из трех чисел: " + max);
