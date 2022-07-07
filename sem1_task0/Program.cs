2d9855943723c7067377047ac5f58be7c9a4c78e
VadimTalimonchik <tvadmins@gmail.com>
2022-07-07 12:12:26 +0300

    seminar 1 task 0 + gitignore

--- sem1_task0/Program.cs
+++ sem1_task0/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 0
+Напишите программу, которая на вход
+принимает число и выдаёт его квадрат,
+например: 4 -> 16; -3 -> 9; -7 -> 49. */
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int summ = numberA * numberA;
+Console.WriteLine(summ);
