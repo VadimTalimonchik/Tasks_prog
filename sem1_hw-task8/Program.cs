0c3ba50e842bfd95c945e3ae0d24ec2d4342d278
VadimTalimonchik <tvadmins@gmail.com>
2022-07-07 17:24:48 +0300

    seminar 1 homework task 8 test

--- sem1_hw-task8/Program.cs
+++ sem1_hw-task8/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 8
+
+Напишите программу, которая на вход
+принимает число (N), а на выходе показывает
+все чётные числа от 1 до N
+*/
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+//for (int i = 1; i < 4; i++) Console.WriteLine(i);
+for (int i = 1; i < numberA; i++);
+if (numberA % 2 == 0)
+{
+    Console.WriteLine(numberA);
+}
+//if (numberA % 2 == 0)
+//    Console.WriteLine("Вы ввели ЧЕТНОЕ число!");
