6c63a24a1136c17ffe72766cbadca9ceed9247d0
VadimTalimonchik <tvadmins@gmail.com>
2022-07-07 14:25:14 +0300

    seminar 1 task 3 solution

--- sem1_task3/Program.cs
+++ sem1_task3/Program.cs
@@ -11,4 +11,74 @@
 7 -> Воскресенье
 */
 
-Console.WriteLine("Hello, World!");
+// Вариант 1
+/*
+Console.WriteLine("Введите номер дня недели: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (numberA == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (numberA == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (numberA == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (numberA == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (numberA == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (numberA == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if (numberA > 7 || numberA < 1)  // && - условное "и"; || - условное "или".
+{
+    Console.WriteLine("Ошибка");
+}
+*/
+
+
+// Вариант 2
+
+Console.WriteLine("Введите номер дня недели: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+    switch (numberA)
+    {
+        case 1:
+            Console.WriteLine("Понедельник");
+            break;
+        case 2:
+            Console.WriteLine("Вторник");
+            break;
+        case 3:
+            Console.WriteLine("Среда");
+            break;
+        case 4:
+            Console.WriteLine("Четверг");
+            break;
+        case 5:
+            Console.WriteLine("Пятница");
+            break;
+        case 6:
+            Console.WriteLine("Суббота");
+            break;
+        case 7:
+            Console.WriteLine("Воскресенье");
+            break;
+        default:
+            Console.WriteLine("Ошибка");
+            break;
+    };
