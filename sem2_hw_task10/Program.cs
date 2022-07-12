5cc2aa565f8b3378f2aab450a4029ec2a0089aa4
VadimTalimonchik <tvadmins@gmail.com>
2022-07-12 17:02:26 +0300

    Seminar 2 homework task 10 solution

---
+++ sem2_hw_task10/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 10
+
+Напишите программу, которая принимает 
+на вход трёхзначное число и на выходе 
+показывает вторую цифру этого числа.
+*/
+
+/*
+// Вариант 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int thirdnumber = num % 10;
+int firstnumber = num / 100;
+int secondnumber = num - (thirdnumber + firstnumber * 100);
+Console.WriteLine(secondnumber / 10);
+*/
+
+
+// Вариант 2
+/*
+int num = new Random().Next(100, 999);
+Console.WriteLine(num);
+int thirdnumber = num % 10;
+int firstnumber = num / 100;
+int secondnumber = num - (thirdnumber + firstnumber * 100);
+Console.WriteLine(secondnumber / 10);
+*/
+
+// Вариант 3
+
+int something (int start, int finish)
+{
+    int num = new Random().Next(100, 999);
+    Console.WriteLine(num);
+    int thirdnumber = num % 10;
+    int firstnumber = num / 100;
+    int secondnumber = num - (thirdnumber + firstnumber * 100);
+    Console.WriteLine(secondnumber / 10);
+    return secondnumber;
+}
+int A = something (100, 999);
