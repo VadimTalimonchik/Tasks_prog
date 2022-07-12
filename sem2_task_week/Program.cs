13ae614c13a575f304bc5db7da35a55234de7ec5
VadimTalimonchik <tvadmins@gmail.com>
2022-07-12 16:08:49 +0300

    Seminar 2 task week solution

---
+++ sem2_task_week/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача с днями недели при помощи массива
+
+int num = Convert.ToInt32(Console.ReadLine());
+string[] weekDays = new string[] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресение" };
+Console.WriteLine(weekDays[num-1]);
