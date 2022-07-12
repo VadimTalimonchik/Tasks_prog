d664061c84cd152bbe85805f433d266e0f19267e
VadimTalimonchik <tvadmins@gmail.com>
2022-07-12 11:18:38 +0300

    Seminar 2 task 11 solution

---
+++ sem2_task11/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 11.
+
+Напишите программу, которая выводит 
+случайное трёхзначное число и удаляет 
+вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+/*
+int numberA = new Random().Next(100, 999);
+Console.WriteLine(numberA);
+int thirdnumber = numberA % 10;
+int firstnumber = numberA / 100;
+Console.WriteLine(thirdnumber + firstnumber * 10);
+*/
+
+// обернутое в функцию
+int something (int start, int finish)
+{
+    int numberA = new Random().Next(start, finish);
+    Console.WriteLine(numberA);
+    int thirdnumber = numberA % 10;
+    int firstnumber = numberA / 100;
+    int result = thirdnumber + firstnumber * 10;
+    return result;
+}
+
+int A = something (100, 999);
+Console.WriteLine(A);
