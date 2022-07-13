cc8bcfdabfc204d46a2e1aff370ad66af73c39dd
VadimTalimonchik <tvadmins@gmail.com>
2022-07-13 16:12:02 +0300

    Seminar 2 homework task 15 solution

---
+++ sem2_hw_task15/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 15
+
+Напишите программу, которая принимает
+на вход цифру, обозначающую день недели,
+и проверяет, является ли этот день выходным.
+*/
+
+Console.Write("Введите число недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string[] weekDays = new string[] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресение" };
+//Console.WriteLine(weekDays[num-1]);
+
+    if (num < 6)
+    {
+        Console.WriteLine("Сегодня " + weekDays[num-1]);
+    }
+    else
+    {
+        Console.WriteLine("Сегодня " + weekDays[num-1] + ", выходной!");
+    }
+    
