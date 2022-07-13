6f1dbfb9733a922602665692b1ed957d566a736d
VadimTalimonchik <tvadmins@gmail.com>
2022-07-13 17:15:54 +0300

    test2

--- sem2_hw_task13/Program.cs
+++ sem2_hw_task13/Program.cs
@@ -5,6 +5,32 @@
 третью цифру заданного числа или
 сообщает, что третьей цифры нет.
 */
+/*
+Console.Write("Введите число недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Привет!");
+string[] weekDays = new string[] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресение" };
+//Console.WriteLine(weekDays[num-1]);
 
+    if (num < 6)
+    {
+        Console.WriteLine("Сегодня " + weekDays[num-1]);
+    }
+    else
+    {
+        Console.WriteLine("Сегодня " + weekDays[num-1] + ", выходной!");
+    }
+    */
+
+int[] myArr = new int[5];
+// Инициализируем каждый элемент массива вручную
+myArr[0] = 100;
+myArr[1] = 23;
+myArr[2] = 25;
+myArr[3] = 31;
+myArr[4] = 1;
+
+foreach (int i in myArr)
+Console.WriteLine(i);
+
+Console.ReadLine();
