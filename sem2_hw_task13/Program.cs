fe5b5f4babfc7415c3cbb09ee9d6659c0331de40
VadimTalimonchik <tvadmins@gmail.com>
2022-07-14 15:49:55 +0300

    Seminar 2 homework task 13 solution

--- sem2_hw_task13/Program.cs
+++ sem2_hw_task13/Program.cs
@@ -5,32 +5,26 @@
 третью цифру заданного числа или
 сообщает, что третьей цифры нет.
 */
-/*
-Console.Write("Введите число недели: ");
-int num = Convert.ToInt32(Console.ReadLine());
 
-string[] weekDays = new string[] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресение" };
-//Console.WriteLine(weekDays[num-1]);
+Console.Write("Введите любое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
 
-    if (num < 6)
+void hasThirdNumber(int x)
+{
+    int div = 1;
+    while (x / div >= 10)
     {
-        Console.WriteLine("Сегодня " + weekDays[num-1]);
+        div *= 10;
+    }
+    if (div < 100)
+    {
+        Console.WriteLine("В числе нет третьей цифры!");
     }
     else
     {
-        Console.WriteLine("Сегодня " + weekDays[num-1] + ", выходной!");
+        int digit = (x / (div / 100)) % 10;
+        Console.WriteLine("Третья цифра в числе: {0}", digit);
     }
-    */
+}
 
-int[] myArr = new int[5];
-// Инициализируем каждый элемент массива вручную
-myArr[0] = 100;
-myArr[1] = 23;
-myArr[2] = 25;
-myArr[3] = 31;
-myArr[4] = 1;
-
-foreach (int i in myArr)
-Console.WriteLine(i);
-
-Console.ReadLine();
+hasThirdNumber(x);
